8be45243ac43f44c84adbde0622be303d1d7151a
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-05-03 21:07:46 +0200

    added on click functionality

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public void pressAnyButton(MouseEvent event){
+        System.out.println("Hello World");
+    }
+
+    @FXML
+    private ImageView imgView;
+
+    @FXML
+    public void handleMouseClick(MouseEvent arg0) {
+        System.out.println("clicked");
+    }
+
 }
