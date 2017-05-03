581954848f6c4dc0bc60c84c39be375a47cd1111
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-05-03 21:15:05 +0200

    Added minimal functionality

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    private ImageView startImgView;
+
+    @FXML
+    public void goToFirstScreen(MouseEvent arg0) {
+        System.out.println("clicked");
+    }
 }
