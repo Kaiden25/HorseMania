fac1b433ec6e85179e0cbfb6637630bbdd64693a
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-05-03 21:33:51 +0200

    Added on click functionality

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,40 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Parent root;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+        root = FXMLLoader.load(getClass().getResource("settings.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
+        PrimaryStage = primaryStage;
+
     }
+
+    private static Stage PrimaryStage;
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @FXML
+    public void goToFirstScreen(MouseEvent arg0) {
+        try{
+            PrimaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("start.fxml")), 900, 600));
+        }
+        catch (Exception ex){
+
+        }
     }
 }
